f5cb1ef27ead022f1312b0133329760cb8f212df
JordanAngst <87876142+JordanAngst@users.noreply.github.com>
2021-07-30 15:51:32 -0400

    Update 170917400_190988840_a04.c

--- 170917400_190988840_a04.c
+++ 170917400_190988840_a04.c
@@ -44,80 +44,87 @@ int main(int argc, char *argv[])
 
 int openFile(char* filename)
 {
-    char content[128];
-    FILE *fp;
-    int totalCustomers;
-    fp = fopen(filename,"r");
+	char content[128];
+	FILE *fp;
+	int totalCustomers;
+	fp = fopen(filename,"r");
 
-    if(fp == NULL)
-    {
-        printf("File not found");
-        return 0;
-    }
-
-    char line[sizeof(content)];
-    while(line = fgetc(fp)) != EOF) 
-    {
-	if (line == '\n') {
-		totalCustomers++;
+	if(fp == NULL)
+	{
+		printf("File not found");
+        	return 0;
 	}
-	//in the event this line of the file is empty
-	else if ((line = fgetc(fp)) == EOF)
-		totalCustomers--;
-    }   
+
+	char line[sizeof(content)];
+
+	while((line[sizeof(content)] = fgetc(fp)) != EOF)
+	{
+		if (*line == '\n')
+		{
+			totalCustomers++;
+		}
+
+		//in the event this line of the file is empty
+		else if ((line[sizeof(content)] = fgetc(fp)) == EOF)
+		{
+			totalCustomers--;
+		}
+	}
+
+	return 0;
 }
 
-int requestResource(int customer_number, int r[]) 
+int requestResource(int customer_number, int r[])
 {
-    // set need = maximum - allocation
-    for (int i = 0; i < p; i++)
-    {
-	for (int j = 0; j < m; j++)
+	// set need = maximum - allocation
+	for (int i = 0; i < p; i++)
 	{
-	    need[i][j] = maximum[i][j] - allocation[i][j];
+		for (int j = 0; j < m; j++)
+		{
+			need[i][j] = maximum[i][j] - allocation[i][j];
+		}
 	}
-    }
+
+	for (int i = 0; i < m; i++)
+	{
+		//if not enough customers
+		if(r[i] > maximum[customer_number][i])
+		{
+			return -1;
+		}
+
+		//if not enough resources
+		else if(r[i] > available[i])
+		{
+			return -1;
+		}
+	}
+
+	//checks if meeting safety standards
+	if(safetyAlgorithm() == 0)
+	{
+		return -1;
+	}
 	
-    for (int i = 0; i < m; i++) 
-    {
-	//if not enough customers
-        if(r[i] > maximum[customer_number][i])
-	{ 
-            return -1;
-        }
-
-	//if not enough resources
-        else if(r[i] > available[i])
+	for(int i = 0; i < p; i++)
 	{
-            return -1;
-        }
-    }
-
-    //checks if meeting safety standards
-    if(safetyAlgorithm() == 0)
-    {
-        return -1;
-    }
-
-    for(int i = 0; i < p; i++)
-    {
-	for (int j = 0; j < m; j++)
-    	{
-	    //add allocation to each customer
-            allocation[customer_number][j] += r[i]; 
+		for (int j = 0; j < m; j++)
+		{
+			//add allocation to each customer
+			allocation[customer_number][j] += r[i];
 	    
-	    //subtract available amount of resource
-            available[i] -= r[i];
+			//subtract available amount of resource
+			available[i] -= r[i];
 		
-	    need[i][j] -= r[i];
+			need[i][j] -= r[i];
+		}
 	}
-    }
-    return 0;
+	return 0;
 }
 
 int releaseResource(int customer_number, int *request)
 {
-    for(int i=0; i < 4; i++)
+    for(int i = 0; i < 4; i++)
     {
 	//add available amount of resource
         available[i] += release[i];
@@ -130,7 +137,7 @@ int releaseResource(int customer_number, int *request)
 
 int safetyAlgorithm()
 {
-    int result;
+	int result;
 
 	result = 0;
 
@@ -176,5 +183,5 @@ int safetyAlgorithm()
 
 void isSafe()
 {
-    printf("State is safe, and request is satisfied");
+	printf("State is safe, and request is satisfied");
 }
